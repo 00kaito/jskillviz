970fc706537e9e3533c39a6027e9c217103542df
dell <bednarz@programmer.net>
2026-04-20 12:21:43 -0400

    Init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'jobs', pathMatch: 'full' },
+  {
+    path: 'upload',
+    loadComponent: () =>
+      import('./features/upload/upload').then((m) => m.UploadComponent),
+  },
+  {
+    path: 'jobs',
+    loadComponent: () =>
+      import('./features/job-list/job-list').then((m) => m.JobListComponent),
+  },
+  { path: '**', redirectTo: 'jobs' },
+];
